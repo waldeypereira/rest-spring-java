398f72b76505c5e361a7388f62cd04f0ab3bc66a
Waldey <waldeypereira@gmail.com>
2025-05-08 16:00:23 +0200

    First steps in Java with Spring Boot 3.4.5

---
+++ src/main/java/com/franjava/rest_spring_java/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.franjava.rest_spring_java.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/com/franjava/rest_spring_java/StartupTests.java
@@ -0,0 +1,13 @@
+package com.franjava.rest_spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
